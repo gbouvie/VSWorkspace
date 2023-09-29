1a373ca7093bd7edbe3ddfd43264024a21d6bb3f
Giovani Bouvie <gbouvie@gmail.com>
2023-09-28 22:40:33 -0300

    Alterações de teste

--- Testes/tabela.js
+++ Testes/tabela.js
@@ -1,13 +1,13 @@
 function onClickMoreInfo(e) {
-    const tableOne = document.getElementById("tableOne");
+    const tableOne = $("#tableOne");
+    const moreInfo = $("#moreInfo");
 
     if (e.classList.contains("fa-circle-plus")) {
-        e.classList.remove("fa-circle-plus");
-        tableOne.style.display = "table";
-        e.classList.add("fa-circle-minus");
+        tableOne.show();
     } else {
-        e.classList.remove("fa-circle-minus");
-        tableOne.style.display = "none";
-        e.classList.add("fa-circle-plus");
+        tableOne.hide();
     }
+
+    moreInfo.toggleClass("fa-circle-plus");
+    moreInfo.toggleClass("fa-circle-minus");
 }
